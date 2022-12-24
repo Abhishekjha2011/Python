9a70afb4ba65ea355d896e4ec804ab9a2e9b2bb7
Abhishekjha2011 <114256000+Abhishekjha2011@users.noreply.github.com>
2022-12-24 19:00:48 +0530

    Display Zero,+ve & -ve

---
+++ display +ve .zero.py
@@ -0,0 +1,14 @@
+#wap to display count of +ve,-ve & zero enter by the user
+
+n=int(input("Enter the N no."))
+while (n!=-1):
+    
+if(n>0):
+    print("The No. is -Ve)
+elif(n<0):
+    print("The No. is +Ve)
+else:
+    print("The No. is Zero")
+
+    print("The no. is =",n)
+    
