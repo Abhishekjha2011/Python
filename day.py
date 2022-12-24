8767eb1f99226aa588944a029432fc971b8a62e8
Abhishekjha2011 <114256000+Abhishekjha2011@users.noreply.github.com>
2022-12-24 18:59:02 +0530

    Finding Days

---
+++ day.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the no. of Day="))
+if n==1:
+    print("The day is Monday=")
+elif n==2:
+    print("The day is Tuesday=")
+elif n==3:
+    print("The day is wednesday=")
+elif n==4:
+    print("The day is Thursday=")
+elif n==5:
+   print("The day is friday=")
+elif n==6:
+    print("The day is saturday=")
+else :
+    print("The day is Sunday=")
