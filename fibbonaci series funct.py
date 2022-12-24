49e2ae6ed5bc36e184e8214189ac9f21299c2512
Abhishekjha2011 <114256000+Abhishekjha2011@users.noreply.github.com>
2022-12-24 18:57:24 +0530

    Fibonacci Series

---
+++ fibbonaci series funct.py
@@ -0,0 +1,16 @@
+#write code using fibonacci series with first 2 terms as 1,2:
+#def fibbon(n):
+n=int(input("Enter any no. to diplay series ="))
+#n3=0
+n1=1
+n2=2
+for i in range(n):
+    
+    n3=n1
+    n2=n3
+    n3=n2+n1
+fibb=n3
+print(fibb)
+    
+    
+
