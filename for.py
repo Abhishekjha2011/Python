f590787ae60aa2ee6cfb9f9745b2ca3abfdff4cb
Abhishek Kr. Jha <114256000+Abhishekjha2011@users.noreply.github.com>
2022-12-27 18:31:31 +0530

    For Loops

---
+++ for.py
@@ -0,0 +1,10 @@
+#sum of first 10 rational no.
+n=int(input("Enter the value of number"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+    print('sum is \t',sum)
+
+
+#range value
+#ex-    #range (20,1,-1)
