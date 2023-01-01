0bc9c558a21fb91c9de6b5b48a4cec0ba33f3475
Abhishek Kr. Jha <114256000+Abhishekjha2011@users.noreply.github.com>
2023-01-01 15:51:06 +0530

    Slicing And Concatination

---
+++ practice slicing concat.py
@@ -0,0 +1,6 @@
+Row= int(input("Enter the Number of row ="))
+Column=int(input("Enter the Number of column ="))
+for i in range(1,Row):
+    for j in range(1,Column):
+        print('*',end='')
+        #print('\n')
