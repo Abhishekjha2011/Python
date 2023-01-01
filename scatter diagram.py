34654fea6a072de854df3f564f4833e7356a944d
Abhishek Kr. Jha <114256000+Abhishekjha2011@users.noreply.github.com>
2023-01-01 15:50:17 +0530

    Scatter Diagram IN Matplot

---
+++ scatter diagram.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x1=np.array([1,4,7,10])
+y1=np.array([10,5,15,20])
+plt.scatter(x1,y1,color='green')
+x2=np.array([5,8,10,12,14])
+y2=np.array([4,12,6,3,0])
+plt.scatter(x2,y2,color='red')
+plt.show()
