6eb5fed9a02312d06a0036281d9e30337f52e396
Abhishek Kr. Jha <114256000+Abhishekjha2011@users.noreply.github.com>
2023-01-01 15:16:31 +0530

    Find Greater No.Using Function

---
+++ greater.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the value of a ="))
+b=int(input("Enter the value of b ="))
+c=int(input("Enter the value of c ="))
+
+if (a>b and a>c):
+    print(" The a is greater ")
+elif (b>c and b>a):
+    print(" The b is greater ")
+else:
+    print("The c is greater ")
+    
+    
