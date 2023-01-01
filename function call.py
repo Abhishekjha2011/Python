6d1b48ef00124b9494b754996a81426bd57b7080
Abhishek Kr. Jha <114256000+Abhishekjha2011@users.noreply.github.com>
2023-01-01 15:09:09 +0530

    Function Calling

---
+++ function call.py
@@ -0,0 +1,18 @@
+# Function without parameter:
+def add():
+   n1=int(input("Enter the first no."))
+   n2=int(input("Enter the second no."))
+   sum=n1+n2
+   print("Ans",sum)
+
+add()
+
+#Function with parameter:
+
+
+def add( n1, n2):
+      sum=n1+n2
+      print("sum is",sum)
+      a=int(input("Enter value of n1"))
+      b=int(input("Enter value of n2"))
+add(a,b)
